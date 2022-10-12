2f696036e707d2defdd57bf87fdc6e6f6ee05470
Vipin Upadhyay <Vipin.Upadhyay@harbingergroup.com>
2022-10-12 20:02:48 +0530

    Add startup.cs for configuring service and pipeline and also some design fixes

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -4,47 +4,23 @@ using WebApplication1.Data;
 using WebApplication1.Repository;
 using WebApplication1.Repository.IRepository;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Hosting;
+using WebApplication1;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-// Add services to the container.
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
-builder.Services.AddAuthentication(options=>options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-{
-    options.LoginPath = "/Login/GoogleLogin";
-}).AddGoogle(option =>
-{
-    option.ClientId = "186608638772-jd94sjg8n3cqposmgcm11dr800imnrin.apps.googleusercontent.com";
-    option.ClientSecret = "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";
-    option.AccessDeniedPath = "/AccessDeniedPath";
-}).AddFacebook(options => {
-    options.AppId = "768398517723364";
-    options.AppSecret = "e0a50b46b60fa8aa19f18f9ee9998761";
-    options.AccessDeniedPath = "/AccessDeniedPathInfo";
-});
-builder.Services.AddScoped<IRegistration, RegistrationRepository>();
-
-var app = builder.Build();
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-//app.UseMvc();
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseAuthentication();
-app.UseAuthorization(); 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using WebApplication1.Data;
+using WebApplication1.Repository.IRepository;
+using WebApplication1.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")
+                ));
+            services.AddAuthentication(options => options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/Login/GoogleLogin";
+                }).AddGoogle(option =>
+                {
+                    option.ClientId = "186608638772-jd94sjg8n3cqposmgcm11dr800imnrin.apps.googleusercontent.com";
+                    option.ClientSecret = "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";
+                    option.AccessDeniedPath = "/AccessDeniedPath";
+                }).AddFacebook(options =>
+                {
+                    options.AppId = "768398517723364";
+                    options.AppSecret = "e0a50b46b60fa8aa19f18f9ee9998761";
+                    options.AccessDeniedPath = "/AccessDeniedPathInfo";
+                });
+            services.AddScoped<IRegistration, RegistrationRepository>();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseMvc();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            // app.MapRazorPages();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute().RequireAuthorization();
+                endpoints.MapRazorPages();
+            });
+
+        }
+    }
+}
